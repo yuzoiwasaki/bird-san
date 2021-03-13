16dc809d30da9147cb6a540d974c0bf008a5f3eb
yuzoiwasaki <yuzoiwasaki0929@gmail.com>
2021-03-13 14:13:57 +0900

    Implement check-out

--- handler.js
+++ handler.js
@@ -11,3 +11,15 @@ module.exports.check_in = async (event) => {
     )
   };
 };
+
+module.exports.check_out = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        "response_type": "in_channel",
+        "text": "お疲れ様でした！"
+      }
+    )
+  };
+};
