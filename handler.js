520d3161ddee07ff15f4f390315846291386f587
yuzoiwasaki <yuzoiwasaki0929@gmail.com>
2021-03-13 11:42:20 +0900

    Fix body text

--- handler.js
+++ handler.js
@@ -3,13 +3,6 @@
 module.exports.check_in = async (event) => {
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    body: 'おはようございます！'
   };
 };
