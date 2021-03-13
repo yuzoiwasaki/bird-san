21b34eaf43c814c2c22a1eaf4e3c02ed7ea9e3d7
yuzoiwasaki <yuzoiwasaki0929@gmail.com>
2021-03-13 17:22:57 +0900

    Call executed user name

--- handler.js
+++ handler.js
@@ -1,24 +1,42 @@
 'use strict';
 
+const qs = require('querystring')
+
+const slack_user_map = {
+  'U01GPV72XQD': 'いわさき',
+  'U01HGSXBLUQ': 'ちなつ',
+  'U01GLB6C7JA': 'けん',
+  'U01GT30MT3L': 'ふくだ',
+  'U01H41J15V4': 'ぱん',
+  'U01J71N9ZCK': 'あきな',
+  'U01KT99P4JZ': 'ねこ'
+}
+
 module.exports.check_in = async (event) => {
+  const parsed_body = qs.parse(event.body)
+  const user_id = parsed_body['user_id']
+
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
         "response_type": "in_channel",
-        "text": "おはようございます！"
+        "text": slack_user_map[user_id] + "さん、おはようございます！:hatched_chick:"
       }
     )
   };
 };
 
 module.exports.check_out = async (event) => {
+  const parsed_body = qs.parse(event.body)
+  const user_id = parsed_body['user_id']
+
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
         "response_type": "in_channel",
-        "text": "お疲れ様でした！"
+        "text": slack_user_map[user_id] + "さん、お疲れ様でした！:clock9:"
       }
     )
   };
